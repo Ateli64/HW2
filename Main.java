0a8bfaac43fa6a176d2d41f937b73b9fce1fe86d
Ateli64 <129958542+Ateli64@users.noreply.github.com>
2025-12-27 12:22:02 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        byte[] nums1 = new byte[]{5, 6, 8};
+        int summ1 = summaArray(nums1);
+        System.out.println("Summa 1: " + summ1);
+
+        byte[] nums2 = new byte[]{5, 7, 3, 7, 6, 8};
+
+        int summ2 = summaArray(nums2);
+        System.out.println("Summa 2: " + summ2);
+    }
+
+    public static int summaArray(byte[] arr) {
+        int summa = 0;
+        for (int i = 0; i < arr.length; i++) {
+            summa += arr[i];
+        }
+        return summa;
+    }
+}
